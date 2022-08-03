23488e0419588220d5f2bd77db11e8c2cf8b8170
user <harshitha2418>
2022-08-03 15:58:28 +0530

    klu

---
+++ sbxkjas.py
@@ -0,0 +1,38 @@
+# class Parent:
+#   def __init__(self, txt):
+#     self.messege = txt
+#
+#   def printmessege(self):
+#     print(self.messege)
+#
+# class Child(Parent):
+#   def __init__(self, txt):
+#     super().__init__(txt)
+#
+# a = Child("Hello, and Welcome!")
+
+# import random
+# n=random.randbytes(3)
+# print(n)
+# print(random.randrange(1,8))
+# print(random.randint(100,211))
+# mylist = ["jadeja","aswin","rahane","shami","dhoni","virat"]
+# mylist1 = ["jadeja","aswin","rahane","shami","dhoni","virat"]
+# print(random.choice(mylist))
+# # print(random.choice(mylist1))
+# random.shuffle(mylist)
+#
+# import re
+# txt = "use of phyton in machine learning"
+# x = re.search("^use.*learning$", txt)
+# if (x):
+#     print("yes! we have a match!")
+# else:
+#     print("no match")
+#
+import re
+txt = "use of phyton in machine learning"
+x = re.findall("in", txt)
+print(x)
+print("the first white-space character is located")
+
